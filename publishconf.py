c19c207fd561ee309d8c569d1b4d51f33d85ad18
Spencer Hill <spencerahill@gmail.com>
2022-09-15 15:01:07 -0400

    try to get disqus working again

--- publishconf.py
+++ publishconf.py
@@ -18,7 +18,4 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "diynamics"
